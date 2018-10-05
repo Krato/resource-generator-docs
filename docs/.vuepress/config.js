28487573cbcd5900e90b0a20709ce317dda06c19
Eric Lagarda <info@ericlagarda.com>
2018-10-05 12:09:00 +0200

    travis build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'Nova Resource Generator',
     description: 'Master Your Universe',
-    base: '/docs/',
+    base: 'resource-generator-docs',
     serviceWorker: true,
     head: [
         [
